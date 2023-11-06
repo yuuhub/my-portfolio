fe93f6e87e642df375d86bd81a2bac0e7a32223f
Yuuri Peñas <jon@gethooked360.com>
2023-11-06 22:37:08 +0800

    Added nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { logo, menu, close } from '../assets';
  
 const Navbar = () => {
   const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState('false');
 
   return (
     <nav
@@ -21,10 +22,52 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <img src={logo} alt='logo' className='w-13 h-10 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer'>Yuuri <span className='sm:block hidden'>| Full Stack Wordpress Developer</span></p>
         </Link>
-      </div>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li 
+              key={link.id}
+              className={`${
+                active === link.title 
+                ? "text-white" 
+                : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img 
+            src={toggle ? close : menu}
+            alt="menu"
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)}
+          />
+          <div className={`${toggle? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
+            <ul className='list-none hidden sm:flex flex-row gap-10'>
+              {navLinks.map((link) => (
+                <li 
+                  key={link.id}
+                  className={`${
+                    active === link.title 
+                    ? "text-white" 
+                    : "text-secondary"
+                  } hover:text-white text-[18px] font-medium cursor-pointer`}
+                  onClick={() => setActive(link.title)}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+        </div>
+      </div> 
     </nav>
   )
 }
