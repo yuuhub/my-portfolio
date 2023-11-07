1a548a64b25b00d8b77bf04083d159e86228677b
Yuuri Peñas <jon@gethooked360.com>
2023-11-07 19:52:20 +0800

    Add api keys for emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,8 +37,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_96j38lw',
+        'template_bbhgupn',
         {
           from_name: form.name,
           to_name: "Yuuri P.",
@@ -46,7 +46,7 @@ const Contact = () => {
           to_email: "yuuriayano@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        '2EnMgFvylD_bh3LFM'
       )
       .then(
         () => {
