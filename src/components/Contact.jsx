7f7cab3be183a87a1eefa99ffa2b5844936c5774
Yuuri Peñas <jon@gethooked360.com>
2023-11-08 12:31:45 +0800

    Change contact form button background

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -120,7 +120,7 @@ const Contact = () => {
 
           <button
             type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            className='bg-[#915eff] py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
           >
             {loading ? "Sending..." : "Send"}
           </button>
